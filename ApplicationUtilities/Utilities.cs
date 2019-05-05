ba07847d04d2d9a99ef55f5bdedcc7f37d1d413c
Syed Moiz Ur Rehman <syedmoizurrehman@hotmail.com>
2019-05-05 12:53:22 +0500

    Add class for thread-safe random number generation.

--- ApplicationUtilities/Utilities.cs
+++ ApplicationUtilities/Utilities.cs
@@ -28,4 +28,15 @@ namespace AppUtilities
             ReplicationFactor = 2;
         }
     }
+
+
+    /// <summary>
+    /// Used for generating thead-safe random numbers. Copied from https://stackoverflow.com/a/1262619
+    /// </summary>
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic] private static Random Local;
+
+        public static Random CurrentThreadsRandom => Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Environment.CurrentManagedThreadId)));
+    }
 }
